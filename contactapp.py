51ffb6a43194bcd6b13a602d166db7b423a451dd
cfaulkner98 <faulkner.chloe@icloud.com>
2025-11-09 22:46:51 +0000

    my starting point
    i added a table and a commit so it stores files permanently, and also prints all rows and certain rows

---
+++ contactapp.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+connection = sqlite3.connect("Contactapp.db")
+cursor = connection.cursor()
+
+#create table
+cursor.execute("create table contacts (number integer, name text)")
+
+
+#information in my table
+release_list = [
+    (78392748291, "family"),
+    (36392738263, "friend"),
+    (272027393729, "chuck")
+              ]
+
+cursor.executemany("insert into contacts values (?, ?)", release_list)
+              
+#store files permanently
+connection.commit()
+
+#print all rows
+for row in cursor.execute("select * from contacts"):
+    print(row)
+
+#print certain rows
+print("*******")
+cursor.execute("select * from contacts where name = :f", {"f": "friend"})
+contacts_search = cursor.fetchall()
+print("contacts_search =", contacts_search)
+
+connection.close()
